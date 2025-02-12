8cefc2f95b2850f42b2b7794ccb10ba07bdcc835
MrF0o <fathihelmi@outlook.com>
2025-02-12 15:03:26 +0100

    fixes

--- server/src/scraper/BrowserManager.ts
+++ server/src/scraper/BrowserManager.ts
@@ -163,7 +163,7 @@ export class BrowserManager {
       }
     }
 
-    const headless = false
+    const headless = true
     this.browser = await puppeteerExtra.launch({
       headless,
       args,
