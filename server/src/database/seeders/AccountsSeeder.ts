90d0e8793db854d75058f68e95f4863df07fd1fa
shibob53 <100147886+shibob53@users.noreply.github.com>
2025-05-07 20:04:05 +0300

    AccountsSeeder.ts

--- server/src/database/seeders/AccountsSeeder.ts
+++ server/src/database/seeders/AccountsSeeder.ts
@@ -5,19 +5,10 @@ import { Seeder } from './SeederInterface'
 export class AccountsSeeder implements Seeder {
   async seed(): Promise<void> {
     const emails1 = [
-      'samir6437@postm.net',
-      'mahmoud1686@postm.net',
-      'mukhtar8115@postm.net',
-      'raed8754@postm.net',
-      'yasir1417@postm.net',
-      'rafeeq5030@postm.net',
-      'adil6193@postm.net',
-      'haitham7187@postm.net',
-      'nawaf9438@postm.net',
-      'ismail1546@postm.net',
+      'shibob53@gmail.com' 
     ]
 
-    const password1 = 'Qq-123123123'
+    const password1 = 'Tr|g,8s`oPw7dOf?_bkR'
 
     // insert all accounts
 
