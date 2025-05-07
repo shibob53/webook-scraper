19babbe3edbf47d4a0b3c511085c0ff1891cfa3c
shibob53 <100147886+shibob53@users.noreply.github.com>
2025-05-07 22:13:14 +0300

    data-source.js

--- server/src/data-source.js
+++ server/src/data-source.js
@@ -5,6 +5,6 @@ export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: 'database.sqlite',
   entities: ['dist/src/entity/*.js'],
-  synchronize: true,    // يجدر حذفها إذا كنت لا تريد تعديل الهيكل تلقائياً
-  logging: false,
+  synchronize: false,    // يجدر حذفها إذا كنت لا تريد تعديل الهيكل تلقائياً
+  logging: true ,
 });
